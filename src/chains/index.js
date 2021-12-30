131621fec5e8a2c3cf350d045fb6df44b2364cea
Arjun <arjun1709@gmail.com>
2021-12-30 14:51:47 +0530

    Integrated Polygon controller

--- src/chains/index.js
+++ src/chains/index.js
@@ -1,7 +1,9 @@
 const ethereum = require('@getsafle/vault-eth-controller');
 const bsc = require('@getsafle/vault-bsc-controller');
+const polygon = require('@getsafle/vault-polygon-controller');
 
 module.exports = {
     ethereum,
     bsc,
+    polygon,
 }
